5e7d0fce3ebdadb522482bd63e5ea38d51160417
Jesper Lykke <62839593+Lykkedk@users.noreply.github.com>
2020-07-12 08:43:47 +0200

    Add files via upload
    Script CamillaDSP.sh & filter.sh for easy change of squeezelite / CamillaDSP filters

---
+++ CamillaDSP.sh
@@ -0,0 +1,144 @@
+#!/bin/sh
+
+FILTER="RP_red_44100.yml"
+FILTER_DESC="0 - DEFAULT filter"
+
+FILTER_1="Filter_test_1"
+FILTER_1_DESC="1 - Description"
+
+FILTER_2="Filter_test_2"
+FILTER_2_DESC="2 - Description"
+
+FILTER_3="Filter_test_3"
+FILTER_3_DESC="3 - Description"
+
+FILTER_4="Filter_test_4"
+FILTER_4_DESC="4 - Description"
+
+FILTER_5="Filter_test_5"
+FILTER_5_DESC="5 - Description"
+
+FILTER_DIR="/home/tc/DSP_Engine/filters/"
+OPTIONS="-p3011"
+DESC="CamillaDSP Engine"
+DAEMON=/home/tc/DSP_Engine/camilladsp
+PIDFILE=/var/run/camilla.pid
+
+case "$1" in
+	start)
+		if [ -f $PIDFILE ]; then
+		echo "$PNAME already running."
+		exit 1
+		fi
+		echo "Starting $DESC: $PNAME..."
+		start-stop-daemon --start --quiet -b -m -p $PIDFILE --exec $DAEMON -- $OPTIONS $FILTER_DIR$FILTER
+		echo "$FILTER enabled :: $FILTER_DESC"
+		;;
+
+	start_1)
+    if [ -f $PIDFILE ]; then
+    echo "$PNAME already running."
+    exit 1
+    fi
+    echo "Starting $DESC: $PNAME..."
+    start-stop-daemon --start --quiet -b -m -p $PIDFILE --exec $DAEMON -- $OPTIONS $FILTER_DIR$FILTER_1
+		echo "$FILTER_1 enabled :: $FILTER_1_DESC"
+		;;
+
+  start_2)
+    if [ -f $PIDFILE ]; then
+    echo "$PNAME already running"
+    exit 1
+    fi
+    echo "Starting $DESC: $PNAME..."
+		start-stop-daemon --start --quiet -b -m -p $PIDFILE --exec $DAEMON -- $OPTIONS $FILTER_DIR$FILTER_2
+		echo "$FILTER_2 enabled :: $FILTER_2_DESC"
+		;;
+
+	start_3)
+    if [ -f $PIDFILE ]; then
+    echo "$PNAME already running."
+    exit 1
+    fi
+    echo "Starting $DESC: $PNAME..."
+    start-stop-daemon --start --quiet -b -m -p $PIDFILE --exec $DAEMON -- $OPTIONS $FILTER_DIR$FILTER_3
+		echo "$FILTER_3 enabled :: $FILTER_3_DESC"
+		;;
+
+  start_4)
+    if [ -f $PIDFILE ]; then
+    echo "$PNAME already running."
+    exit 1
+    fi
+    echo "Starting $DESC: $PNAME..."
+    start-stop-daemon --start --quiet -b -m -p $PIDFILE --exec $DAEMON -- $OPTIONS $FILTER_DIR$FILTER_4
+    echo "$FILTER_4 enabled :: $FILTER_4_DESC"
+		;;
+
+	start_5)
+		if [ -f $PIDFILE ]; then
+		echo "$PNAME already running."
+	 	exit 1
+	 	fi
+	 	echo "Starting $DESC: $PNAME..."
+	 	start-stop-daemon --start --quiet -b -m -p $PIDFILE --exec $DAEMON -- $OPTIONS $FILTER_DIR$FILTER_5
+	 	echo "$FILTER_5 enabled :: $FILTER_5_DESC"
+		;;
+
+	stop)
+		if [ ! -f $PIDFILE ]; then
+		echo "$PNAME is not running."
+		exit 1
+		fi
+		echo "Stopping $DESC: $PNAME..."
+		# We need to kill squeezelite. CamillaDSP allways have to be started first duo to squeezelite is blocking ALSA
+		sudo killall squeezelite
+		sudo killall squeezelite-custom
+		start-stop-daemon --stop --quiet -p $PIDFILE
+		sudo rm -f $PIDFILE
+		;;
+
+	restart)
+		echo "Restarting $DESC..."
+		$0 stop
+		sleep 1
+		$0 start
+		;;
+
+	status)
+		if [ -f $PIDFILE ]; then
+		PID=`cat $PIDFILE`
+
+		PIDS=`/bin/busybox pgrep $DAEMON | awk '{printf "%s ", $1}'`
+
+		for GOTPID in $PIDS; do
+		if [ x"$GOTPID" = x"$PID" ]; then
+		echo "$PNAME is running. PID=$PID"
+		exit 0
+		fi
+		done
+		fi
+
+		echo "$PNAME not running."
+		exit 1
+		;;
+	*)
+
+		echo -e "Usage: $0 [start|stop|restart|status]"
+		echo
+		echo "====== Filter description ======"
+		echo $FILTER $FILTER_DESC
+		echo $FILTER_1 $FILTER_1_DESC
+		echo $FILTER_2 $FILTER_2_DESC
+		echo $FILTER_3 $FILTER_3_DESC
+		echo $FILTER_4 $FILTER_4_DESC
+		echo $FILTER_5 $FILTER_5_DESC
+		echo "================================"
+		echo
+
+		exit 1
+		;;
+
+esac
+
+exit 0
---
+++ filter.sh
@@ -0,0 +1,195 @@
+#!/bin/sh
+
+# ========================================== #
+#  Squeezelite / CamillaDSP starter script!  #
+#  ***     Easy change of filters       ***  #
+#  JesperLykke (lykkedk) 2020                #
+#                                            #
+#  Execute with /home/tc/filter.sh nofilter  #
+#  Execute with /home/tc/filter.sh filter_1  #
+#  Execute with /home/tc/filter.sh filter_2  #
+#  Execute with /home/tc/filter.sh filter_3  #
+#  Execute with /home/tc/filter.sh filter_4  #
+#  Execute with /home/tc/filter.sh filter_5  #
+# ========================================== #
+
+case "$1" in
+nofilter)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+echo "Starting squeezelite with no filters"
+sleep 0.5
+# Enter the correct string below for starting squeezelite without filters
+sudo squeezelite -n NO_DSP_DAC -o sound_out -a 160:4::1 -b 10000:20000 -U -U -z
+# =======================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+filter)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+sleep 0.5
+echo "Starting CamillaDSP with DEFAULT filter @ loopback"
+sudo /home/tc/CamillaDSP.sh start
+echo "Starting squeezelite @ loopback (with filters)"
+sleep 0.5
+# Enter the correct string below for starting squeezelite with filters
+sudo /mnt/mmcblk0p2/tce/squeezelite-custom -n DSP_DAC -o squeeze -a 160:4::1 -b 10000:20000 -r 44100-192000:2500 -U -U -z
+# ====================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+filter_1)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+sleep 0.5
+echo "Starting CamillaDSP with filter @ loopback"
+sudo /home/tc/CamillaDSP.sh start_1
+echo "Starting squeezelite @ loopback (with filters)"
+sleep 0.5
+# Enter the correct string below for starting squeezelite with filters
+sudo /mnt/mmcblk0p2/tce/squeezelite-custom -n DSP_DAC -o squeeze -a 160:4::1 -b 10000:20000 -r 44100-192000:2500 -U -U -z
+# ====================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+filter_2)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+sleep 0.5
+echo "Starting CamillaDSP with filter @ loopback"
+sudo /home/tc/CamillaDSP.sh start_2
+echo "Starting squeezelite @ loopback (with filters)"
+sleep 0.5
+# Enter the correct string below for starting squeezelite with filters
+sudo /mnt/mmcblk0p2/tce/squeezelite-custom -n DSP_DAC -o squeeze -a 160:4::1 -b 10000:20000 -r 44100-192000:2500 -U -U -z
+# ====================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+filter_3)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+sleep 0.5
+echo "Starting CamillaDSP with filter @ loopback"
+sudo /home/tc/CamillaDSP.sh start_3
+echo "Starting squeezelite @ loopback (with filters)"
+sleep 0.5
+# Enter the correct string below for starting squeezelite with filters
+sudo /mnt/mmcblk0p2/tce/squeezelite-custom -n DSP_DAC -o squeeze -a 160:4::1 -b 10000:20000 -r 44100-192000:2500 -U -U -z
+# ====================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+filter_4)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+sleep 0.5
+echo "Starting CamillaDSP with filter @ loopback"
+sudo /home/tc/CamillaDSP.sh start_4
+echo "Starting squeezelite @ loopback (with filters)"
+sleep 0.5
+# Enter the correct string below for starting squeezelite with filters
+sudo /mnt/mmcblk0p2/tce/squeezelite-custom -n DSP_DAC -o squeeze -a 160:4::1 -b 10000:20000 -r 44100-192000:2500 -U -U -z
+# ====================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+filter_5)
+echo
+sleep 0.5
+echo "Kill squeezelite + squeezelite-custom"
+sudo killall squeezelite 2>/dev/null
+sudo killall squeezelite-custom 2>/dev/null
+sleep 0.5
+echo "Kill CamillaDSP"
+sudo /home/tc/CamillaDSP.sh stop 2>/dev/null
+sudo killall camilladsp 2>/dev/null
+echo
+sleep 0.5
+echo "Starting CamillaDSP with filter @ loopback"
+sudo /home/tc/CamillaDSP.sh start_5
+echo "Starting squeezelite @ loopback (with filters)"
+sleep 0.5
+# Enter the correct string below for starting squeezelite with filters
+sudo /mnt/mmcblk0p2/tce/squeezelite-custom -n DSP_DAC -o squeeze -a 160:4::1 -b 10000:20000 -r 44100-192000:2500 -U -U -z
+# ====================================================================
+echo
+ps aux | grep squeezelite
+ps aux | grep squeezelite-custom
+ps aux | grep camilladsp
+;;
+
+help)
+/home/tc/CamillaDSP.sh
+;;
+
+esac
+
+exit 0
